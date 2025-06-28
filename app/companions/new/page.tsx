dec42981c53256c66cf530ff396f0be065f9d588
Iskandar <iskandarqosimov120@gmail.com>
2025-06-28 14:33:20 +0500

    initial commit

---
+++ app/companions/new/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CompanionsNew = () => {
+  return (
+    <div>CompanionsNew</div>
+  )
+}
+
+export default CompanionsNew
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className=''>Welcome to 1st Page</h1>
+      <Button> Register</Button>
+    </div>
+  )
+}
+
+export default Page
